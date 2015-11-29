f5d0d49bc4d39d6b544de50ead379f050b3f3abc
vlad101 <V_efros85@hotmail.com>
2015-11-29 09:11:25 -0500

    front end adjustments

--- client/app/myBoard/myBoard.controller.js
+++ client/app/myBoard/myBoard.controller.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('workspaceApp')
-  .controller('MyBoardCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('MyBoardCtrl', function ($scope, $http) {
+
+    // Get user from session
+    $http.get('/api/sessions/user')
+      .then(function successCallback(userInfo) {
+          $scope.userId = userInfo.data._id;
+        }, function errorCallback(response) {
+          $scope.userId = 'Something went wrong, try again.';
+    }).catch( function() {
+      $scope.userId = '';
+    });
+
+});

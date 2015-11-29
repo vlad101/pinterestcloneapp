ac9f03f4a6f18ff3b887b4a199847ef137404580
vlad101 <vladimir.efros@gmail.com>
2015-11-29 01:40:50 +0000

    added user model boilerplate

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/users', require('./api/user'));
   app.use('/auth/twitter', require('./api/auth'));
   app.use('/api/things', require('./api/thing'));
   
